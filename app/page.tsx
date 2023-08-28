24ac00537c603479da29e2677bdfbf4f5f01f094
richard <richard.zsupos@gmail.com>
2023-08-29 00:25:45 +0200

    Set the tittle to the center

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,26 @@ export default function Home() {
   return (
     <main className="landing-page">
       <div className="stalker">
-        <div className={`${boo.className} main-box`}>
-          <h3 className="director">Andrei Tarkovsky&#39;s</h3>
-          <h1 className="title">STALKER</h1>
+        <div className="top">
+          <div className={`${boo.className} main-box`}>
+            <h3 className="director">Andrei Tarkovsky&#39;s</h3>
+            <h1 className="title">STALKER</h1>
+          </div>
         </div>
+        <div className="bottom">
+          <div className="do">
+            <a href="#">
+              <span className="visually-hidden">Scroll down</span>
+              <svg className="circle" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                  <ellipse className="background" ry="60" rx="60" cy="62.5" cx="62.5" stroke-width="1"/>
+                  <ellipse className="foreground" ry="60" rx="60" cy="62.5" cx="62.5" stroke-width="2"/>
+                </g>
+              </svg>
+            </a>
+          </div>
+        </div>
+        
       </div>
     </main>
   )
