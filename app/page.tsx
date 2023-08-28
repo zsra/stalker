df036d509ef0e6b422ec8f1d2c32c221dc77dbc7
richard <richard.zsupos@gmail.com>
2023-08-28 23:13:25 +0200

    Change font style

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import './styles/home.css'
+import { Black_Ops_One } from 'next/font/google'
+
+const boo = Black_Ops_One({ weight: "400", subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="landing-page">
-      <div className="main-box">
-        <h3 className="director">Andrei Tarkovsky&#39;s</h3>
-        <h1 className="title">STALKER</h1>
+      <div className="stalker">
+        <div className={`${boo.className} main-box`}>
+          <h3 className="director">Andrei Tarkovsky&#39;s</h3>
+          <h1 className="title">STALKER</h1>
+        </div>
       </div>
     </main>
   )
