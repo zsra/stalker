a41ebe2ac9e34bc2619bcf8c047eee1fd83c3f72
richard <richard.zsupos@gmail.com>
2023-09-05 22:23:59 +0200

    Add cast html structure

--- src/pages/cast.tsx
+++ src/pages/cast.tsx
@@ -1,7 +1,27 @@
+import Image from 'next/image' 
+
 export default function Cast() {
     return (
-        <section>
-           
+        <section id="cast" className="cast">
+            <h3>Cast</h3>
+            <div className="actor-container">
+                <div className="actor-description"></div>
+                <div className="actor-image-container">
+                    <Image src="../public/images/cast/Aleksandr_Kaydanovskiy.jpg" alt='Aleksandr Kaydanovskiy' />
+                </div>
+            </div>
+            <div className="actor-container">      
+                <div className="actor-image-container">
+                    <Image src="../public/images/cast/Anatoly_Solonitsyn.jpg" alt='Anatoly_Solonitsyn' />
+                </div>
+                <div className="actor-description"></div>
+            </div>
+            <div className="actor-container">
+                <div className="actor-description"></div>
+                <div className="actor-image-container">
+                    <Image src="../public/images/cast/Nikolai_Grinko.jpg.jpg" alt='Nikolai Grinko' />
+                </div>
+            </div>
         </section>
     )
 }
